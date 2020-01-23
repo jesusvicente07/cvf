0fcb021b53e9012a36ec47b84f0373c9d0c433df
jesusvicente07 <jesusvicente007@gmail.com>
2020-01-22 18:33:46 -0700

    cambios

--- resources/views/competitions/add_competitions.blade.php
+++ resources/views/competitions/add_competitions.blade.php
@@ -23,37 +23,36 @@
                         </div>
                     </div>
                         
-                        <form class="m-form m-form--fit m-form--label-align-left" style="text-align:left">
+                        <form action="{{ route('storecompetitions') }}" method="POST" class="m-form m-form--fit m-form--label-align-left" style="text-align:left">
                         @csrf
                             <div class="m-portlet__body">
                                 <div class="form-group m-form__group">
                                     <label>Nombre</label>
-                                    <input type="text" class="form-control m-input"  placeholder="e.g. Evaluación WISC IV"> 
+                                    <input type="text" name="name" class="form-control m-input {{ $errors->has('name') ? 'is-danger' : '' }} "  placeholder="e.g. Evaluación WISC IV" value="{{ old('name') }}" autocomplete="off">
+                                    @error('name')
+                                      <div class="text-red">{{ $errors->first('name') }}</div>
+                                    @enderror   
                                 </div>
                                 <div class="m-form__group">
                                     <label>Cursos que conforman la competencia</label>
                                     <div class="form-inline">
-                                        <input type="text" class="form-control m-input"  placeholder="Nombre"> 
-                                        <input type="text" class="form-control m-input mr-3"  placeholder="Enlace"> 
-                                        <a class="btn btn-primary mr-5"><i class="fa fa-plus"></i></a>
+                                        <input type="text" name="course_name" class="form-control m-input {{ $errors->has('courses') ? 'is-danger' : '' }}" placeholder="Nombre" value="{{ old('course_name') }}" autocomplete="off"> 
+                                        <input type="text" name="link" class="form-control m-input mr-3 {{ $errors->has('courses') ? 'is-danger' : '' }}" placeholder="Enlace" value="{{ old('link') }}" autocomplete="off"> 
+                                        <div class="btn btn-primary mr-4" id="addcompetition"><i class="fa fa-plus"></i></div>
                                     </div>
+                                    @error('courses')
+                                      <div class="text-red">{{ $errors->first('courses') }}</div>
+                                    @enderror   
                                 </div>
                                 <div class="m-form__group">
                                     <table class="table table-hover text-center">
                                         <tbody>
-                                            <tr>
-                                                <th scope="row">Udemy - Como evaluar</th>
-                                                <td>http://udemy.com/mi-curso</td>
-                                                <td><a href="#" class="text-body"><i class="fa fa-trash" style="font-size:150%"></i></a></td>
-                                            </tr>
-                                            <tr>
-                                                <th scope="row">Khan Academy - curso 2</th>
-                                                <td>http://khanacademy.org/mi-curso</td>
-                                                <td><a href="#" class="text-body"><i class="fa fa-trash" style="font-size:150%"></i></a></td>
-                                            </tr>
                                         </tbody>
                                     </table>  
                                 </div>
+                                    <div class="m-form__group" style="text-align: right;">
+                                        <input type="submit" class="btn btn-primary"  value="Guardar">
+                                    </div>
                             </div>
                         </form>
                 </div>
@@ -63,5 +62,20 @@
 @endsection
 
 @section('customScripts')
-
+<script>
+var count_courses=0;
+$('#addcompetition').click(function(){
+    let course_name = $("input[name='course_name']").val();
+    let link = $("input[name='link']").val();
+    if(course_name && link)
+    {
+        let tbody = "<tr><td><input hidden  name='courses["+ count_courses +"][name]' value='"+ course_name +"'> " + course_name + "</td><td><input hidden  name='courses["+ count_courses +"][link]' value='"+ link +"'> " + link + "</td><td><a class='delete' class='text-body'><i class='fa fa-trash' style='font-size:150%'></i></a></td></tr>";
+        $("table tbody").append(tbody);
+        count_courses++;
+    }
+});
+$("table tbody").on("click", ".delete", function() {
+   $(this).closest("tr").remove();
+});
+</script>
 @endsection
