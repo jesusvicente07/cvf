a705ce493059f494d5b0a844006858aa986bf591
jesusvicente07 <jesusvicente007@gmail.com>
2020-01-31 18:19:29 -0700

    coordinadores

--- app/Http/Controllers/CoordinatorController.php
+++ app/Http/Controllers/CoordinatorController.php
@@ -7,10 +7,12 @@ use Illuminate\Http\Request;
 class CoordinatorController extends Controller
 {
     public function coordinators(){
+        $coordinators="";
         return view('coordinators.list_coordinators', compact('coordinators'));
     }
 
     public function addcoordinators(){
+        $coordinators="";
         return view('coordinators.add_coordinators', compact('coordinators'));
     }
 }
