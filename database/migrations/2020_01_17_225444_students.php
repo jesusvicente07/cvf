d3ee81f732ac0da12c5f907a44d5f75c2f05754c
jan7alberto <jan7alberto@gmail.com>
2020-02-17 18:20:14 -0700

    nueva migracion

--- database/migrations/2020_01_17_225444_students.php
+++ database/migrations/2020_01_17_225444_students.php
@@ -39,20 +39,6 @@ class Students extends Migration
 
         });
 
-        Schema::create('student_courses', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedBigInteger('student_id');
-            $table->unsignedBigInteger('course_id');
-            $table->string('evidence',150)->nullable();
-            $table->boolean('status')->nullable();
-            $table->timestamps();
-
-            $table->unique(['student_id','course_id']);
-
-            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
-            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
-
-        });
     }
 
     /**
--- database/migrations/2020_01_21_010320_courses.php
+++ database/migrations/2020_01_21_010320_courses.php
@@ -22,6 +22,21 @@ class Courses extends Migration
 
             $table->foreign('competition_id')->references('id')->on('competitions')->onDelete('cascade');
         });
+
+        Schema::create('student_course', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('course_id');
+            $table->string('evidence',150)->nullable();
+            $table->boolean('status')->nullable();
+            $table->timestamps();
+
+            $table->unique(['student_id','course_id']);
+
+            $table->foreign('student_id')->references('id')->on('students')->onDelete('cascade');
+            $table->foreign('course_id')->references('id')->on('courses')->onDelete('cascade');
+
+        });
     }
 
     /**
