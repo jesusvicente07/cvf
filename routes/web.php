75ff7e66a8c776bb269c08280203d35aaf7aa0de
jan7alberto <jan7alberto@gmail.com>
2020-03-11 14:43:26 -0700

    rutas

--- routes/web.php
+++ routes/web.php
@@ -23,6 +23,8 @@ Route::post('/nuevo/coordinador', 'CoordinatorController@store')->name('storecoo
 Route::get('/editar/coordinador/{coordinator}', 'CoordinatorController@editcoordinators')->name('editcoordinators');
 Route::put('/editar/coordinador/{coordinator}', 'CoordinatorController@updatecoordinators')->name('updatecoordinators');
 Route::delete('eliminar/coordinador/{id}', 'CoordinatorController@deletecoordinators')->name('deletecoordinators');
+Route::delete('/eliminar/detalleUsuarioCarrera/{coordinator}', 'CoordinatorController@deleteDetailUserCareer')->name('deleteDetailUserCareer');
+
 
 Route::post('/users/login', 'Auth\LoginController@userLogin');
 
@@ -35,13 +37,25 @@ Route::post('/nuevo/estudiante', 'StudentController@store')->name('storestudents
 Route::get('/editar/estudiante/{student}', 'StudentController@editstudents')->name('editstudents');
 Route::put('/editar/estudiante/{student}', 'StudentController@updatestudents')->name('updatestudents');
 Route::delete('eliminar/estudiante/{id}', 'StudentController@deletestudents')->name('deletestudents');
+Route::post('evidencias', 'StudentTrajectories@studentsevidences')->name('studentsevidences');
+Route::post('descargar/evidencia/{idS}/{idC}', 'StudentController@downloadevidence')->name('downloadevidence');
+
 
 Route::get('/mi/progreso/{student}', 'StudentTrajectories@myprogress')->name('myprogress');
 Route::post('/selecionar/trayectorias', 'StudentTrajectories@storetrajectories')->name('storeStudentTrajectories');
 Route::get('/trayectorias/selecionadas', 'StudentTrajectories@studenttrajectories')->name('studenttrajectories');
 Route::get('/selecionar/trayectorias', 'StudentTrajectories@selecttrajectories')->name('selecttrajectories');
 Route::delete('/eliminar/trayectoria/selecionada/{id}', 'StudentTrajectories@deletestudenttrajectories')->name('deletestudenttrajectories');
+Route::delete('eliminar/evidencia/{id}', 'StudentTrajectories@deleteevidences')->name('deleteevidences');
+Route::get('obtener/evidencia/{idS}/{idC}', 'StudentTrajectories@getevidence')->name('getevidence');
 
+
+Route::get('/cursos', 'CourseController@courses')->name('courses');
+Route::get('/nuevo/curso', 'CourseController@addcourses')->name('addcourses');
+Route::post('/nuevo/curso', 'CourseController@storecourses')->name('storecourses');
+Route::get('/editar/curso/{course}', 'CourseController@editcourses')->name('editcourses');
+Route::put('/editar/curso/{course}', 'CourseController@updatecourses')->name('updatecourses');
+Route::delete('eliminar/curso/{id}', 'CourseController@deletecourses')->name('deletecourses');
 
 Route::get('/competencias', 'CompetitionController@competitions')->name('competitions');
 Route::get('/nueva/competencia', 'CompetitionController@addcompetitions')->name('addcompetitions');
@@ -49,7 +63,7 @@ Route::post('/nueva/competencia', 'CompetitionController@store')->name('storecom
 Route::get('/editar/competencia/{competition}', 'CompetitionController@editcompetitions')->name('editcompetitions');
 Route::put('/editar/competencia/{competition}', 'CompetitionController@update')->name('updatecompetitions');
 Route::delete('eliminar/competencia/{id}', 'CompetitionController@deletecompetitions')->name('deletecompetitions');
-Route::delete('eliminar/curso/{id}', 'CompetitionController@deletecourses')->name('deletecourses');
+Route::delete('eliminar/competenciaC/{id}', 'CompetitionController@deletecompetitionsC')->name('deletecompetitionsC');
 
 Route::get('/carreras', 'CareerController@careers')->name('careers');
 Route::get('/nueva/carrera', 'CareerController@addcareers')->name('addcareers');
