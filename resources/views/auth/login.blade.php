d22a613ac15a6fce27d56f93b4e206f9d66d440d
jesusvicente07 <jesusvicente007@gmail.com>
2020-03-12 15:16:37 -0700

    CambiosLogin

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -48,12 +48,6 @@
                                 </div>
                             </form>
                         </div>
-                        <div class="m-login__account">
-                            <span class="m-login__account-msg">
-                                Don't have an account yet ?
-                            </span>&nbsp;&nbsp;
-                            <a href="/register" id="m_login_signup" class="m-link m-link--light m-login__account-link">Sign Up</a>
-                        </div>
                     </div>
                 </div>
             </div>
